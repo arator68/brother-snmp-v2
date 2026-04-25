8ced86d74fa93c2257fbbcde2b01f3a1e20f81ea
arator68 <arator68@users.noreply.github.com>
2026-04-25 18:57:49 +0200

    prepare for hacs

--- custom_components/brother_snmp_v2/__init__.py
+++ custom_components/brother_snmp_v2/__init__.py
@@ -1,8 +1,17 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 DOMAIN = "brother_snmp_v2"
 
-async def async_setup(hass, config):
+
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
 
-async def async_setup_entry(hass, entry):
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
