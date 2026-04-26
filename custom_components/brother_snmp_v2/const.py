d20281fdc93de6bed0432a1d35689857b6325d6f
arator68 <arator68@users.noreply.github.com>
2026-04-26 12:30:26 +0200

    Update const.py

--- custom_components/brother_snmp_v2/const.py
+++ custom_components/brother_snmp_v2/const.py
@@ -6,3 +6,11 @@ CONF_COMMUNITY = "community"
 IDENTITY_OID = "1.3.6.1.4.1.2435.2.3.9.1.1.7.0"
 
 SCANNER_BASE_OID = "1.3.6.1.4.1.2435.2.3.9"
+
+IMPORTANT_KEYWORDS = [
+    "toner",
+    "jam",
+    "count",
+    "page",
+    "status",
+]
