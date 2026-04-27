cf661c3f602fe6c5d3172cb84549b388ca0b2097
arator68 <arator68@users.noreply.github.com>
2026-04-27 10:54:25 +0200

    sync

--- custom_components/brother_snmp_v2/snmp.py
+++ custom_components/brother_snmp_v2/snmp.py
@@ -1,48 +1,57 @@
-import asyncio
 import logging
+from datetime import timedelta
 
-from pysnmp.hlapi.asyncio import nextCmd, SnmpEngine
-from pysnmp.hlapi.asyncio import (
-    CommunityData,
-    UdpTransportTarget,
-    ContextData,
-    ObjectType,
-    ObjectIdentity,
-)
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.util import dt as dt_util
+
+from pysnmp.hlapi import *
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def snmp_walk(host, community, base_oid):
+def _walk(host, community, base_oid):
     result = {}
 
-    try:
-        async for (errorIndication,
-                   errorStatus,
-                   errorIndex,
-                   varBinds) in nextCmd(
-            SnmpEngine(),
-            CommunityData(community),
-            UdpTransportTarget((host, 161)),
-            ContextData(),
-            ObjectType(ObjectIdentity(base_oid)),
-            lexicographicMode=False,
-        ):
+    for (errorIndication,
+         errorStatus,
+         errorIndex,
+         varBinds) in nextCmd(
+        SnmpEngine(),
+        CommunityData(community),
+        UdpTransportTarget((host, 161)),
+        ContextData(),
+        ObjectType(ObjectIdentity(base_oid)),
+        lexicographicMode=False,
+    ):
 
-            if errorIndication:
-                _LOGGER.error(f"SNMP error: {errorIndication}")
-                return {}
+        if errorIndication or errorStatus:
+            return {}
 
-            if errorStatus:
-                _LOGGER.error(f"SNMP error: {errorStatus}")
-                return {}
-
-            for varBind in varBinds:
-                oid, value = varBind
-                result[str(oid)] = str(value)
-
-    except Exception as e:
-        _LOGGER.error(f"SNMP WALK failed: {e}")
-        return {}
+        for varBind in varBinds:
+            oid, value = varBind
+            result[str(oid)] = str(value)
 
     return result
+
+
+class BrotherCoordinator(DataUpdateCoordinator):
+    def __init__(self, hass, host, community):
+        super().__init__(
+            hass,
+            _LOGGER,
+            name="Brother SNMP Scanner",
+            update_interval=timedelta(seconds=15),
+        )
+
+        self.host = host
+        self.community = community
+
+    async def _async_update_data(self):
+        walk = await self.hass.async_add_executor_job(
+            _walk,
+            self.host,
+            self.community,
+            "1.3.6.1.4.1.2435.2.3.9"
+        )
+
+        return {"walk": walk}
