05d66af271779721bdefc9b5240dfc207ec00dcc
arator68 <arator68@users.noreply.github.com>
2026-04-27 12:43:35 +0200

    sync

--- custom_components/brother_snmp_v2/__init__.py
+++ custom_components/brother_snmp_v2/__init__.py
@@ -23,3 +23,15 @@ async def async_setup_entry(hass, entry):
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
+@property
+def device_info(self):
+    base = self.coordinator.serial_number or self.coordinator.host
+
+    return DeviceInfo(
+        identifiers={(DOMAIN, base)},
+        name=self.coordinator.model or "Brother Device",
+        manufacturer="Brother",
+        model=self.coordinator.model,
+        serial_number=self.coordinator.serial_number,  # kann None sein!
+    )
