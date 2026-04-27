6a35df109f2993d8411fd2946924bfd0afbf7bcb
arator68 <arator68@users.noreply.github.com>
2026-04-27 10:22:48 +0200

    sync

--- custom_components/brother_snmp_v2/snmp.py
+++ custom_components/brother_snmp_v2/snmp.py
@@ -1,41 +1,37 @@
 import asyncio
+import subprocess
 import logging
-
-from pysnmp.hlapi import (
-    SnmpEngine,
-    CommunityData,
-    UdpTransportTarget,
-    ContextData,
-    ObjectType,
-    ObjectIdentity,
-    nextCmd,
-)
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def snmp_walk(host, community, base_oid):
-    result = {}
+    loop = asyncio.get_event_loop()
 
-    def _walk():
-        for (errorIndication,
-             errorStatus,
-             errorIndex,
-             varBinds) in nextCmd(
-                SnmpEngine(),
-                CommunityData(community),
-                UdpTransportTarget((host, 161)),
-                ContextData(),
-                ObjectType(ObjectIdentity(base_oid)),
-                lexicographicMode=False,
-        ):
-            if errorIndication or errorStatus:
-                return {}
+    def run():
+        try:
+            result = subprocess.check_output(
+                ["snmpwalk", "-v2c", "-c", community, host, base_oid],
+                stderr=subprocess.DEVNULL,
+                timeout=10,
+            ).decode()
 
-            for varBind in varBinds:
-                oid, value = varBind
-                result[str(oid)] = str(value)
+            data = {}
 
-        return result
+            for line in result.splitlines():
+                if "=" not in line:
+                    continue
 
-    return await asyncio.get_event_loop().run_in_executor(None, _walk)
+                oid, val = line.split("=", 1)
+                oid = oid.strip()
+                val = val.split(":", 1)[-1].strip()
+
+                data[oid] = val
+
+            return data
+
+        except Exception as e:
+            _LOGGER.error(f"SNMP WALK failed: {e}")
+            return {}
+
+    return await loop.run_in_executor(None, run)
